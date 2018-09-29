4b8784020f21383f48ec63d53398c7b7ed5c62b7
Alessandro Piazza <apiazza134@gmail.com>
2018-09-29 21:24:07 +0200

    General fix

--- analisi.py
+++ analisi.py
@@ -6,11 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+from fitting import *
+
 # Disclaimer #
 print('Ricordati le le variabili siano compatibili con il file *.txt\n')
 
 ## Load data and name variables #
-data = np.loadtxt('data/data.txt', unpack = True)
+data = np.loadtxt('...', unpack = True)
 
 x = data[0]
 dx = data[1]
@@ -37,20 +39,23 @@ def f(x, p0, p1):
     return p0 + p1*x
     
 def gradf(x, p0, p1):
-    return [p1, 1, x]       # dfdx, dfdp0, dfdp
+    return [p1, 1., x]       # dfdx, dfdp0, dfdp
+    
+def dfdx(x, p0, p1):
+    return gradf(x, p0, p1)[0]
 
 def sigmaf2(x, popt, pcov):
     dfdx, dfdp0, dfdp1 = gradf(x, *popt)
-    return pcov[0][0]*(dfdp0**2) + pcov[1][1]*(dfdp0**2) + 2*dfdp0*dfdp1*pcov[0][1]
+    return abs(pcov[0][0]*(dfdp0**2) + pcov[1][1]*(dfdp0**2) + 2*dfdp0*dfdp1*pcov[0][1])
     
 ## Initial parameters
 pinit = [2., 1.]
 
 '''
 ## Data plot with f(x; pinit)
-figInit = figData
-axInit = axData
 t = np.linspace(x.min(), x.max(), 10*N)
+figInit, axInit = plt.subplots(1, 1)
+axInit.errorbar(x, y, yerr=dy, xerr=dx, fmt='', ecolor='k', capsize=None, linestyle='', label='data')
 axInit.plot(t, f(t, *pinit), linewidth = 0.7, color='b', label='$ f(x; \mathrm{p}_{\mathrm{init}}) $')
 axInit.set_title("Test with $ f(x; \mathrm{p}_{\mathrm{init}}) $")
 axInit.legend()
@@ -76,13 +81,16 @@ for i in range(len(s)):
         pcovn[j, i] = pcovn[i, j]   # pcov and pcovn should be symmetric matrices
 
 chi2 = (((y - f(x, *popt))/dy)**2).sum()
+#chi2Eff = (((y - f(x, *popt))**2)/(dy**2 + dfdx(x, *popt)**2 * dx**2)).sum()
+
 dof = N - len(popt)
 chi2n = chi2/dof
+#chi2Effn = chi2Eff/dof
 
 
 ## Vertical residuals
-res = y - f(x, *popt)
-resMean = res.mean()
+resn = (y - f(x, *popt))/dy
+resnMean = resn.mean()
 
 
 ## Output fit results
@@ -100,42 +108,49 @@ print(pcovn, '\n')
 
 print('Chisquare = %f, degrees of freedom = %f' % (chi2, dof))
 print('Normalized chi square = %f' % chi2n)
+#print('Chisquare = %f, degrees of freedom = %f' % (chi2Eff, dof))
+#print('Normalized chi square = %f' % chi2Effn)
 
-print('\nResiduals mean = %f' %resMean)
+print('\nNormalized residuals mean = %f' %resnMean)
 
 print('\n## END ##\n')
 
 
-## Data plot with f(x; pinit)
-#epsilon = np.abs(x.max() - x.min())/(len(x)) * 1e-1
-#X = np.linspace(x.min() - epsilon, x.max() + epsilon, len(x)*1000)
-t = np.linspace(x.min(), x.max(), 10*N)
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+## Data plot with f(x; pinit)
+''' Plot best fit function slightly before and after max and min data '''
+epsilon = (np.abs(x.max() - x.min())/N) * 0.5
+
+''' LaTeX style, makes data plot very slow '''
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif', size=11)
 
 figFit, axFit = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios':[3, 1]})
 figFit.subplots_adjust(hspace=0)
 
+# Plot data
 axFit[0].errorbar(x, y, yerr=dy, xerr=dx, fmt='', ecolor='k', capsize=None, linestyle='', label='data')
-axFit[0].plot(t, f(t, *popt), linewidth = 0.7, color='b', label='$ f(x; \mathrm{p}_{\mathrm{opt}}) $')
-axFit[0].plot(t, f(t, *popt) - np.sqrt(sigmaf2(t, popt, pcov)), linewidth = 0.7, color='r', linestyle='--')
-axFit[0].plot(t, f(t, *popt) + np.sqrt(sigmaf2(t, popt, pcov)), linewidth = 0.7, color='r', linestyle='--')
-axFit[0].set_title("Test with $ f(x; \mathrm{p}_{\mathrm{opt}}) $")
-axFit[0].set_xlabel("$ x \ [\mathrm{au}] $")
-axFit[0].set_ylabel("$ y \ [\mathrm{au}] $")
+
+# Plot best fit curve (and confidence band)
+t = np.linspace(x.min() - epsilon, x.max() + epsilon, 10*N)
+axFit[0].plot(t, f(t, *popt), linewidth = 0.7, color='b', label='$ I = \hat{I}_0 + \Delta V / \hat{R} $')
+#axFit[0].plot(t, f(t, *popt) - np.sqrt(sigmaf2(t, popt, pcov)), linewidth = 0.7, color='r', linestyle='--')
+#axFit[0].plot(t, f(t, *popt) + np.sqrt(sigmaf2(t, popt, pcov)), linewidth = 0.7, color='r', linestyle='--')
+axFit[0].set_title("Data plot with best-fit curve and normalized residuals")
+axFit[0].set_xlabel("$ \Delta V \ [\mathrm{V}] $")
+axFit[0].set_ylabel("$ I \ [\mathrm{mA}] $")
 axFit[0].legend(bbox_to_anchor=(1, 1))
 axFit[0].minorticks_on()
 axFit[0].grid(which = 'major', linestyle = '--')
 
-axFit[1].errorbar(x, res, yerr=dy, xerr=None, fmt='', ecolor='k', capsize=None, linestyle='', label='residuals')
-axFit[1].plot(t, np.full(len(t), resMean), linewidth = 0.7, color='r', label='$ res $ mean')
-axFit[1].set_xlabel("$ x \ [\mathrm{au}] $")
-axFit[1].set_ylabel("$ res \ [\mathrm{au}] $")
+# Plot residuals
+axFit[1].errorbar(x, resn, yerr=dy, xerr=None, fmt='', ecolor='k', capsize=None, linestyle='', label='norm. residuals')
+axFit[1].plot(t, np.full(len(t), resnMean), linewidth = 0.7, color='r', label='$ resn $ mean')
+axFit[1].set_xlabel("$ \Delta V \ [\mathrm{V}] $")
+axFit[1].set_ylabel("$ resn \ [-] $")
 axFit[1].legend(bbox_to_anchor=(1, 1))
 axFit[1].minorticks_on()
 axFit[1].grid(which = 'major', linestyle = '--')
 
-figFit.savefig('dataFit.png', dpi = 1200, bbox_inches='tight')
+#figFit.savefig('dataFit.png', dpi = 1200, bbox_inches='tight')
 plt.show()
-
--- fitting.py
+++ fitting.py
@@ -1,10 +1,13 @@
-import numpy as np
-import os
-import pylab
+# !/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
+## Import libraries
+import numpy as np
 from scipy.optimize import curve_fit
 from scipy.odr import odrpack
 
+
+##
 def sample_statistics(sample):
     ''' Calculate the sample mean and (unbaised) sample standard deviation using np. Also return mean standard deviation.
     ''' 
@@ -12,73 +15,74 @@ def sample_statistics(sample):
     s = np.sqrt(sample.var(ddof = 1))
     
     return m, s, s/np.sqrt(len(sample))
+  
     
-def weighted_average(y, dy):
+## Media pesata e errore sulla media
+def weighted_average(data, dataerr):
     ''' Fit with constant model, i.e. weighted average of a list of mesurements.
     '''
-    if (len(y) != len(dy)):
+    if (len(data) != len(dataerr)):
         return print('\nErrore, array di dimensioni non corrette.\n')
-    w = (1/dy)**2
+    w = (1/dataerr)**2
     S = w.sum()
-    q = (w*y).sum()/S
+    q = (w*data).sum()/S
     dq = np.sqrt(1/S)
-    chisq = ((((y - q)/dy))**2).sum()
-    chisqn = chisq/(len(y) - 1)
-    return q, dq, chisqn
+    chisq = ((((data - q)/dataerr))**2).sum()
+    return q, dq, chisq
+ 
     
-def linearOLS(x, y, dy):
-    ''' Ordinary least square for linear model y = m*x + q.
+## Ordinalry least squares analitico per modello lineare
+def linearOLS(xdata, ydata, yerr):
+    ''' Ordinary least square for linear model y = q + m*x. Popt is [q, m]
     '''
-    w = (1/dy)**2
+    w = (1/yerr)**2
     Sx0 = w.sum()
-    Sx1 = (w*x).sum()
-    Sx2 = (w*x*x).sum()
-    Sxy0 = (w*y).sum()
-    Sxy1 = (w*x*y).sum()
+    Sx1 = (w*xdata).sum()
+    Sx2 = (w*xdata*xdata).sum()
+    Sxy0 = (w*ydata).sum()
+    Sxy1 = (w*xdata*ydata).sum()
     D = Sx0*Sx2 - Sx1*Sx1
     
     m = (Sxy1*Sx0 - Sxy0*Sx1)/D
     q = (Sxy0*Sx2 - Sxy1*Sx1)/D
-    popt = np.array([m, q])
+    popt = np.array([q, m])
     
     dm2 = Sx0/D
     dq2 = Sx2/D
     cov_mq = - Sx1/D
-    pcov = np.array(([dm2, cov_mq], [cov_mq, dq2]))
+    pcov = np.array(([dq2, cov_mq], [cov_mq, dm2]))
     
     return popt, pcov
+
     
-def OLSev(f, dfdx, x, y, dx, dy, par, cov, absolute_sigma = True, conv_diff = 1e-7, max_cycles = 10, **kw):
+## Curve_fit iterato con errori efficaci
+def curve_fitEff(f, dfdx, xdata, ydata, xerr, yerr, pInit, absolute_sigma=True, conv_diff=1e-7, max_cycles=10, **kw):
     cycles = 1
+    pInit, pcovInit = curve_fit(f, xdata, ydata, p0=pinit, sigma=dy, absolute_sigma=absolute_sigma, **kw)
     while True:
         if cycles >= max_cycles:
             cycles = -1
             break
-        dyeff = np.sqrt(dy**2 + (dfdx(x, *par) * dx)**2)
-        npar, ncov = curve_fit(f, x, y, p0 = par, sigma = dyeff, absolute_sigma = absolute_sigma, **kw)
-        perror = abs(npar - par) / npar
-        cerror = abs(ncov - cov) / ncov
-        par = npar
-        cov = ncov
+        dyeff = np.sqrt(yerr**2 + (dfdx(xdata, *pinit) * xerr)**2)
+        popt, pcov = curve_fit(f, xdata, ydata, p0=pInit, sigma=dyeff, absolute_sigma=absolute_sigma, **kw)
+        poptError = abs(popt - pInit) / popt
+        pcovError = abs(pcov - pcovInit) / pcov
+        pInit = popt
+        pcovInit = pcov
         cycles += 1
-        if (perror < conv_diff).all() and (cerror < conv_diff).all():
+        if (poptError < conv_diff).all() and (pcovError < conv_diff).all():
             break
     print(cycles)
-    return par, cov
+    return popt, pcov
 
-def fitScipyNLLS(x, y, dy, func, pars):
-    """ Fit a series of data points with scipy (function must be in from f(x, p0, ...))
-    """
-    popt, pcov = curve_fit(func, x, y, p0 = pars, sigma = dy, absolute_sigma = True)
-    perr = np.sqrt(pcov.diagonal())
-    return popt, perr
 
-def fitScipyODR(x, y, dx, dy, func, pars):
+## Fit con orthogonal distance regressione (ODR)
+def fitScipyODR(xdata, ydata, xerr, yerr, func, pInit):
     """ Fit a series of data points with ODR (function must be in from f(beta[n], x))
     """
     model = odrpack.Model(func)
-    data = odrpack.RealData(x, y, sx = dx, sy = dy)
-    odr = odrpack.ODR(data, model, beta0 = pars)
+    data = odrpack.RealData(xdata, ydata, sx=xerr, sy=yerr)
+    odr = odrpack.ODR(data, model, beta0=pInit)
     out = odr.run()
     popt = out.beta
     pcov = out.cov_beta
@@ -86,6 +90,8 @@ def fitScipyODR(x, y, dx, dy, func, pars):
     print('Chi square = %f' % out.sum_square)
     return popt, pcov
 
+
+'''
 def fit(engine, x, y, dx, dy, f, dfdx, pars, cov = None):
     """ Common interface to all the fit engines.
     """
@@ -124,5 +130,5 @@ def fitLinear(engine, x, y, dx, dy, pars):
         return fitScipyODR(x, y, dx, dy, pol1ODR, pars)
     else:
         sys.exit('Unkown fit engine %s.' % engine)
-        
+'''        
         
